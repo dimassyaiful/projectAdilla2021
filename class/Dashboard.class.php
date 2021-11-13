972e822744427c92487bc33acb5048f1ac7378aa
Dimpuls <41774933+dimassyaiful@users.noreply.github.com>
2021-11-13 18:36:32 +0700

    Update Dashboard.class.php

--- class/Dashboard.class.php
+++ class/Dashboard.class.php
@@ -14,9 +14,36 @@ class Dashboard
         $this->conn = $database->getConnection();
     }
 
+    // public function GetDataChartImport()
+    // {
+    //     $this->sql = "SELECT COUNT(*) as `total`, MONTHNAME(i.date) as `bulan`,MONTH(i.date), `type` FROM tbl_invoices i, tbl_invoice_items it WHERE i.id= it.id_invoice and it.type = 'import' GROUP BY MONTH(i.date) ASC";
+    //     $this->statement = $this->conn->prepare($this->sql);
+    //     $this->statement->execute();
+    //     $datas = array();
+
+    //     while ($result = $this->statement->fetch(PDO::FETCH_OBJ)) :
+    //         $datas[] = $result;
+    //     endwhile;
+    //     return $datas;
+    // }
+
+    // public function GetDataChartExport()
+    // {
+    //     $this->sql = "SELECT COUNT(*) as `total`, MONTHNAME(i.date) as `bulan`,MONTH(i.date), `type` FROM tbl_invoices i, tbl_invoice_items it WHERE i.id= it.id_invoice and it.type = 'export' GROUP BY MONTH(i.date) ASC";
+    //     $this->statement = $this->conn->prepare($this->sql);
+    //     $this->statement->execute();
+    //     $datas = array();
+
+    //     while ($result = $this->statement->fetch(PDO::FETCH_OBJ)) :
+    //         $datas[] = $result;
+    //     endwhile;
+    //     return $datas;
+    // }
+
+
     public function GetDataChartImport()
     {
-        $this->sql = "SELECT COUNT(*) as `total`, MONTHNAME(`date`) as `bulan`,MONTH(`date`), `type` FROM tbl_invoices WHERE `type` = 'Import' GROUP BY MONTH(`date`) ASC";
+        $this->sql = "SELECT COUNT(*) as `total`, MONTHNAME(`dateOfPib`) as `bulan`,MONTH(`dateOfPib`) FROM tbl_import GROUP BY MONTH(`dateOfPib`) ASC";
         $this->statement = $this->conn->prepare($this->sql);
         $this->statement->execute();
         $datas = array();
@@ -26,10 +53,11 @@ class Dashboard
         endwhile;
         return $datas;
     }
+
 
     public function GetDataChartExport()
     {
-        $this->sql = "SELECT COUNT(*) as `total`, MONTHNAME(`date`) as `bulan`,MONTH(`date`), `type` FROM tbl_invoices WHERE `type` = 'Export' GROUP BY MONTH(`date`) ASC";
+        $this->sql = "SELECT COUNT(*) as `total`, MONTHNAME(`dateOfPib`) as `bulan`,MONTH(`dateOfPib`) FROM tbl_export GROUP BY MONTH(`dateOfPib`) ASC";
         $this->statement = $this->conn->prepare($this->sql);
         $this->statement->execute();
         $datas = array();
@@ -39,4 +67,5 @@ class Dashboard
         endwhile;
         return $datas;
     }
+ 
 }
